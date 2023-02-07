78215acc568af5760407cc4b560d39779cec9381
YagoCastello <yagodealmeidacs@gmail.com>
2023-02-07 12:28:57 -0300

    modifiquei o arquivo main

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #Our main file.
+
+import speech_recognition as sr
+
+#Cria um reconhecedor
+r = sr.Recognizer()
+
+#Abrir o microfone para captura
+with sr.Microphone() as source:
+    
+    audio = r.listen(source) #Define microgone como fonte de audio
+
+    print(r.recognize_google(audio))
+
